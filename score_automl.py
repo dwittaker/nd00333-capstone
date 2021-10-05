8a9fe6263d4201c9ee2c0a415a6bceff10863a9c
Duane Wittaker <dwittaker@gmail.com>
2021-10-04 22:52:32 -0600

    Add files via upload

--- score_automl.py
+++ score_automl.py
@@ -3,25 +3,26 @@ import logging
 import json
 import numpy
 import joblib
+# from sklearn.externals import joblib
 import azureml.train.automl
 from azureml.core.model import Model
-
+########## Need to remember that this is running on the inference remote machine. Check if the model path would be the same
 def init():
     """
     This function is called when the container is initialized/started, typically after create/update of the deployment.
     You can write the logic here to perform init operations like caching the model in memory
     """
-    #     global model
-    
-    #     model_path = os.path.join(
-    #     # os.getenv("AZUREML_MODEL_DIR"), "sklearn_regression_model.pkl"
-    #     os.getenv("AZUREML_MODEL_DIR"), "model.pkl"
-    #     )
-    #     # deserialize the model file back into a sklearn model
-    #     model = joblib.load(model_path)
-    global automl_model
-    automl_model_path = Model.get_model_path('automl_model')
-    automl_model = joblib.load(automl_model_path)
+    global model
+
+    model_path = os.path.join(
+    # os.getenv("AZUREML_MODEL_DIR"), "sklearn_regression_model.pkl"
+    os.getenv("AZUREML_MODEL_DIR"), "model.pkl"
+    )
+    # deserialize the model file back into a sklearn model
+    model = joblib.load(model_path)
+    #     global automl_model
+    #     automl_model_path = Model.get_model_path('automl_model')
+    #     automl_model = joblib.load(automl_model_path)
     
 
     logging.info("Init complete")
@@ -37,7 +38,7 @@ def run(raw_data):
         logging.info("Request received")
         data = json.loads(raw_data)["data"]
         data = numpy.array(data)
-        result = automl_model.predict(data)
+        result = model.predict(data)
         logging.info("Request processed")
         return result.tolist()
     except Exception as e:
@@ -45,3 +46,4 @@ def run(raw_data):
         return error
 
 # https://docs.microsoft.com/en-us/azure/machine-learning/how-to-machine-learning-interpretability-automl
+# https://docs.microsoft.com/en-us/azure/machine-learning/how-to-deploy-advanced-entry-script
--- score_hdrive.py
+++ score_hdrive.py
@@ -4,16 +4,20 @@ import numpy as np
 import pandas as pd
 import joblib
 from azureml.core.model import Model
-
+# This explains the use of azure model dir vs get model path
+# https://docs.microsoft.com/en-us/azure/machine-learning/how-to-deploy-advanced-entry-script
 dict_colmap = {}
 json_file_path = "dict_colmap.txt"
-
+########## Need to remember that this is running on the inference remote machine. Check if the model path would be the same
 def init():
     global model
     print ("model initialized" + time.strftime("%H:%M:%S"))
-#     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'RF-AutoPrices_hdrive_model.joblib' #'model.pt')
-    model_root = Model.get_model_path('RF-AutoPrices')
-    model = joblib.load(os.path.join(model_root, 'RF-AutoPrices_hdrive_model.joblib'))
+#     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'RF-AutoPrices_hdrive_model.joblib') #'model.pt'
+#     model = joblib.load(model_path)
+    
+    model_path = Model.get_model_path('RFHD-AutoPrices')
+#     model = joblib.load(os.path.join(model_root, 'RF-AutoPrices_hdrive_model.joblib'))
+    model = joblib.load(model_path)
     
     global dict_colmap
     with open(json_file_path, 'r') as j:
